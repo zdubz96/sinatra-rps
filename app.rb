f5513e17acadc504a01badda6c7d37d368317103
zdubz96 <zchan@chicagobooth.edu>
2025-04-14 16:02:33 +0000

    1

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
+key={1=>"rock",2=>"paper",3=>"scissors"}
+outcome=""
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  @our_play="rock"
+  @their_play=key[rand(1..3)]
+  if @our_play==@their_play
+    @outcome="tied"
+  elsif @their_play=="paper"
+    @outcome="lost"
+  else
+    @outcome="won"
+  end
+  erb(:outcome)
+end
+
+get("/paper") do
+  @our_play="paper"
+  @their_play=key[rand(1..3)]
+  if @our_play==@their_play
+    @outcome="tied"
+  elsif @their_play=="scissors"
+    @outcome="lost"
+  else
+    @outcome="won"
+  end
+  erb(:outcome)
+end
+
+get("/scissors") do
+  @our_play="scissors"
+  @their_play=key[rand(1..3)]
+  if @our_play==@their_play
+    @outcome="tied"
+  elsif @their_play=="rock"
+    @outcome="lost"
+  else
+    @outcome="won"
+  end
+  erb(:outcome)
 end
